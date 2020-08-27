36cb443be0f1ed048800eb78dc75108fb3ae9cb1
jero <aneurysmjs@outlook.com>
2020-08-28 02:41:04 +0300

    refactor(home): add initial grid layout

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 
 import './Home.scss';
 
+const cells = Array(10)
+  .fill({ id: '' })
+  .map((o, i) => ({
+    ...o,
+    id: i,
+  }));
+
 function Home(): React.ReactElement {
-  return <div className="container">Home</div>;
+  return (
+    <div className="home__container">
+      <div className="home-grid">
+        {cells.map(({ id }) => (
+          <div key={id} className="home-grid__item">
+            <div>item {id + 1}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
