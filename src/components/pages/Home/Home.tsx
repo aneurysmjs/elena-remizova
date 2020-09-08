d35310641f5bdce5b2c000b5f5711f049f7ea163
jero <aneurysmjs@outlook.com>
2020-09-09 01:27:43 +0300

    refactor(home): import all images from pics.ts

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -2,72 +2,20 @@ import React from 'react';
 
 import './Home.scss';
 
-import img1 from '~/assets/img/1.jpg';
-import img2 from '~/assets/img/2.jpg';
-import img3 from '~/assets/img/3.jpg';
-import img4 from '~/assets/img/4.jpg';
-import img5 from '~/assets/img/5.jpg';
-import img6 from '~/assets/img/6.jpg';
-import img7 from '~/assets/img/7.jpg';
-
-const cells = [
-  {
-    id: 0,
-    img: img1,
-    title: 'Some Creative Title',
-    description: 'Using responsive utility variants to build adaptive user interfaces.',
-  },
-  {
-    id: 1,
-    img: img2,
-    title: 'Some Creative Title',
-    description: 'This works for every utility class in the framework',
-  },
-  {
-    id: 2,
-    img: img3,
-    title: 'Some Creative Title',
-    description: 'By default, Tailwind uses a mobile first breakpoint system',
-  },
-  {
-    id: 3,
-    img: img4,
-    title: 'Some Creative Title',
-    description: 'Examples like this intentionally do not react to changing your browser size',
-  },
-  {
-    id: 4,
-    img: img5,
-    title: 'Some Creative Title',
-    description: 'Targeting mobile screens',
-  },
-  {
-    id: 5,
-    img: img6,
-    title: 'Some Creative Title',
-    description: 'Tailwind\'s breakpoints only include a min-width and don\'t include a max-width',
-  },
-  {
-    id: 6,
-    img: img7,
-    title: 'Some Creative Title',
-    description: 'Customizing breakpoints',
-  },
-];
+import pics from './pics';
 
 const Home: React.FunctionComponent = () => {
   return (
     <div className="home__container">
       <div className="home-grid">
-        {cells.map(({ id, img, description, title }) => (
+        {pics.map(({ id, img, title }) => (
           <div key={id} className="home-grid__item">
             <div className="home-grid__mask">
               <div>
                 <h3 className="mb-2 text-center">{title}</h3>
-                <p>{description}</p>
               </div>
             </div>
-            <img src={img} alt={img} />
+            <img className="home-grid__image" src={img} alt={img} />
           </div>
         ))}
       </div>
---
+++ src/components/pages/Home/pics.ts
@@ -0,0 +1,39 @@
+import img1 from '~/assets/img/work/elena-remizova-paint-mountains-landscape.jpg';
+import img2 from '~/assets/img/work/elena-remizova-paint-guatape.jpg';
+import img3 from '~/assets/img/work/elena-remizova-paint-portrait-jero.jpg';
+import img4 from '~/assets/img/work/elena-remizova-paint-portrait-masha.jpg';
+import img5 from '~/assets/img/work/elena-remizova-paint-portrait-mother.jpg';
+import img6 from '~/assets/img/work/elena-remizova-paint-portrait-woman.jpg';
+import img7 from '~/assets/img/work/elena-remizova-paint-road-to-mountains.jpg';
+import img8 from '~/assets/img/work/elena-remizova-paint-village-lake.jpg';
+import img9 from '~/assets/img/work/elena-remizova-paint-оktyabrskaya-street.jpg';
+import img10 from '~/assets/img/work/elena-remizova-paint-birch.jpg';
+import img11 from '~/assets/img/work/elena-remizova-paint-forest.jpg';
+import img12 from '~/assets/img/work/elena-remizova-paint-lupines.jpg';
+import img13 from '~/assets/img/work/elena-remizova-paint-nizhny.jpg';
+import img14 from '~/assets/img/work/elena-remizova-paint-portrait-baks.jpg';
+import img15 from '~/assets/img/work/elena-remizova-paint-portrait-friend.jpg';
+import img16 from '~/assets/img/work/elena-remizova-paint-portrait-makcim.jpg';
+import img17 from '~/assets/img/work/elena-remizova-paint-portrait-naked-woman.jpg';
+import img18 from '~/assets/img/work/elena-remizova-paint-river.jpg';
+
+export default [
+  { id: 0, img: img1, title: 'Городской пейзаж' },
+  { id: 1, img: img2, title: 'Гуатапе' },
+  { id: 2, img: img3, title: 'Джеро' },
+  { id: 3, img: img4, title: 'Маша' },
+  { id: 4, img: img5, title: 'Галина' },
+  { id: 5, img: img6, title: 'Женщина' },
+  { id: 6, img: img7, title: 'Дорога горам' },
+  { id: 7, img: img8, title: 'Годоское осеро' },
+  { id: 8, img: img9, title: '' },
+  { id: 9, img: img10, title: '' },
+  { id: 10, img: img11, title: '' },
+  { id: 11, img: img12, title: '' },
+  { id: 12, img: img13, title: 'Нижний Новгород' },
+  { id: 13, img: img14, title: 'Бакс' },
+  { id: 14, img: img15, title: '' },
+  { id: 15, img: img16, title: '' },
+  { id: 16, img: img17, title: '' },
+  { id: 17, img: img18, title: '' },
+];
