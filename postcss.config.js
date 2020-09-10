8783166d89330ff443e3db8615c0d8231dcc4286
jero <aneurysmjs@outlook.com>
2020-09-10 20:48:39 +0300

    build(postcss): install postcss-import plugin

--- postcss.config.js
+++ postcss.config.js
@@ -4,10 +4,12 @@
  */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const tailwindcss = require('tailwindcss');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const postCssImport = require('postcss-import');
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
-const plugins = [];
+const plugins = [postCssImport];
 plugins.push(tailwindcss);
 // plugins.push(tailwindcss('tailwind.config.js'))
 // This is if you want to include your custom config
