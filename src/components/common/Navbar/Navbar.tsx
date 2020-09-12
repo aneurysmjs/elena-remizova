45890e56fd5d45a6cdfa327b02f254870ffd1c5f
jero <aneurysmjs@outlook.com>
2020-09-12 18:44:21 +0300

    refactor(navbar): set styles and layout

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
-
-import Button from '~/components/common/Button';
+import { NavLink } from 'react-router-dom';
 
 import './Navbar.scss';
 
+const links = [
+  {
+    id: 0,
+    name: 'Главная',
+    path: '/',
+  },
+  {
+    id: 1,
+    name: 'Портфолио',
+    path: '/portfolio',
+  },
+];
+
 const Navbar: React.FunctionComponent = () => (
-  <nav className="navbar">
-    <Button>Home</Button>
-    <Button>Portfolio</Button>
+  <nav className="navbar container">
+    <div className="navbar__navigation">
+      {links.map(({ id, name, path }) => (
+        <NavLink
+          key={id}
+          className="navbar-link focus:outline-none"
+          activeClassName="navbar-link--active"
+          to={path}
+          exact
+        >
+          {name}
+          <span className="navbar-link__bar" />
+        </NavLink>
+      ))}
+    </div>
   </nav>
 );
 
