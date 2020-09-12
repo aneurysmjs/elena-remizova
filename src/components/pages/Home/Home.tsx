05555ea38c37dfe939e88e07d98ea3720475526a
jero <aneurysmjs@outlook.com>
2020-09-12 19:35:24 +0300

    feat(home): display <Gallery />

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './Home.scss';
 
-import Gallery from '~/components/common/Gallery';
-
 const Home: React.FunctionComponent = () => {
   return (
-    <div className="home-container mb-2">
-      <Gallery />
-    </div>
+    <section className="home">
+      <article className="home__content">
+        <h1 className="home__title">Елена Ремизова</h1>
+        <p className="home__description">
+          Я художник, вы можете посмотреть все мои работы{' '}
+          <NavLink to="/portfolio" className="no-underline hover:underline text-teal-500">
+            здесь
+          </NavLink>
+        </p>
+      </article>
+    </section>
   );
 };
 
