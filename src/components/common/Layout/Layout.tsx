5afdc70fc4450428be14260fa3fe25e3a5eccad4
jero <aneurysmjs@outlook.com>
2020-09-12 18:26:02 +0300

    refactor(layout): add overall padding

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -10,7 +10,7 @@ interface PropsType {
 const Layout = (props: PropsType): React.ReactElement => (
   <React.Fragment>
     <Navbar />
-    <main className="bg-white pt-4 min-h-screen z-10 relative">{props.children}</main>
+    <main className="bg-white pt-4 min-h-screen z-10 relative px-4">{props.children}</main>
     <Footer />
   </React.Fragment>
 );
